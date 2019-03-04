dabfb75ec6dce19b8bc1905057f4bdb92db1e76d
mezzelfo <luca.mezzelfo@gmail.com>
2019-03-04 09:49:49 +0100

    Creation of STANDARD CODE CHALLENGE
    Incluso il file tool.c che contiene delle funzioni di aiuto che normalmente sono richieste

---
+++ STANDARD CODE CHALLENGE/tool.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void throwError(const char* errmsg)
+{
+	fprintf(stderr, "%s\n", errmsg);
+	exit(EXIT_FAILURE);
+}
+
+void* secMalloc(const size_t s)
+{
+	void* ptr = malloc(s);
+	if (ptr == NULL)
+		throwError("Errore durante l'allocazione della memoria");
+	return ptr;
+}
+
+int readInt(FILE* f)
+{
+	int n;
+	if(fscanf(f,"%d",&n) != 1)
+		throwError("Errore durante la lettura del file");
+	return n;
+}
+
+unsigned readUnsigned(FILE* f)
+{
+	unsigned n;
+	if(fscanf(f,"%u",&n) != 1)
+		throwError("Errore durante la lettura del file");
+	return n;
+}
